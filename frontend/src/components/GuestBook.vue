6c8093e79681d467bd5a32f0ecbecf8b7741924e
NektoUa <illyabarkov@gmail.com>
2022-02-18 10:39:18 +0200

    add vuetifyjs

---
+++ frontend/src/components/GuestBook.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam temporibus
+      similique sit cum eum corrupti officia, dolore quam repudiandae commodi.
+      Accusamus voluptate incidunt vero tenetur sunt unde laborum dicta! Minima!
+    </p>
+    <form>
+      <v-btn color="secondary" elevation="2">Add</v-btn>
+    </form>
+  </div>
+</template>
