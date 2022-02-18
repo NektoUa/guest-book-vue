231f021bb0db091cf79faeb825e84e2ce12ed619
NektoUa <illyabarkov@gmail.com>
2022-02-18 16:01:36 +0200

    fix bags

--- frontend/src/components/guest-form/GuestForm.vue
+++ frontend/src/components/guest-form/GuestForm.vue
@@ -3,15 +3,39 @@
     <h1>Please write your opinion</h1>
     <v-form v-model="valid">
       <v-col cols="12" md="6">
-        <Name />
-        <Email />
-        <Textarea />
+        <v-text-field
+          v-model="person.nickname"
+          :rules="nameRules"
+          :counter="10"
+          label="Nickname"
+          required
+        ></v-text-field>
+
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+        ></v-text-field>
+        <small id="emailHelp" class="form-text text-muted"
+          >We'll never share your email with anyone else.</small
+        >
+
+        <label for="person.text">Your opinion</label>
+        <v-textarea
+          :rules="opinionRules"
+          required
+          solo
+          name="person.text"
+          label="Write your opinion"
+        ></v-textarea>
+
         <v-select :items="items" label="Star"></v-select>
         <v-checkbox
           v-model="checkbox"
           :label="`Agree with the rules : ${checkbox.toString()}`"
         ></v-checkbox>
-        <v-btn color="secondary" elevation="2">Add</v-btn>
+        <v-btn color="secondary" elevation="2" @click="changeForm">Add</v-btn>
         <span> {{ person.name }}</span>
       </v-col>
     </v-form>
@@ -19,17 +43,9 @@
 </template>
 
 <script>
-import Name from "./form-templates/Name.vue";
-import Email from "./form-templates/Email.vue";
-import Textarea from "./form-templates/Textarea.vue";
 import { eventEmitter } from "../../main";
 
 export default {
-  components: {
-    Name,
-    Email,
-    Textarea,
-  },
   data: () => ({
     person: {
       nickname: "",
@@ -41,24 +57,13 @@ export default {
     checkbox: false,
     opinionRules: [(v) => !!v || "Field is empty"],
   }),
-  created() {
-    eventEmitter.$on("yourName", (name) => {
-      this.person.nickname = name;
-    });
-    eventEmitter.$on("yourEmail", (email) => {
-      this.person.email = email;
-    });
-    eventEmitter.$on("yourText", (text) => {
-      this.person.text = text;
-    });
-  },
 
-  //   methods: {
-  //     changeForm() {
-  //       if (this.checkbox) {
-  //         eventEmitter.$emit("yourAnswer", this.person.nickname);
-  //       }
-  //     },
-  //   },
+  methods: {
+    changeForm() {
+      if (this.checkbox) {
+        eventEmitter.$emit("yourAnswer", this.person.nickname);
+      }
+    },
+  },
 };
 </script>
